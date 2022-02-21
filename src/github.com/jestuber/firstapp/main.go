c8e406230c8f62e8a14d4ad06427463ad559d816
jestuber <jestuber@gmail.com>
2022-02-21 10:52:11 -0800

    strings and runes

--- src/github.com/jestuber/firstapp/main.go
+++ src/github.com/jestuber/firstapp/main.go
@@ -5,30 +5,18 @@ import (
 )
 
 func main() {
-	var n bool
-	fmt.Printf("%v, %T\n", n, n)
-	fmt.Println(n == false)
+	s := "this is a string"
+	fmt.Printf("%v, %T\n", s, s)
+	fmt.Printf("%v, %T\n", s[2], s[2])
+	fmt.Printf("%v, %T\n", string(s[2]), s[2])
 
-	m := 42
-	fmt.Printf("%v, %T\n", m, m)
-	m = 1e10
+	s2 := "this is also a string"
+	fmt.Printf("%v, %T\n", s+s2, s+s2)
 
-	var k uint16 = 42
-	fmt.Printf("%v, %T\n", k, k)
+	b := []byte(s) // byte slice
+	fmt.Printf("%v, %T\n", b, b)
 
-	p := 3.14
-	p = 13.7e72 //this is too large for float32, so go initializes p as a float64
-	p = 2.1e14
-	fmt.Printf("%v, %T\n", p, p)
+	r := 'a' //runes are type aliases for int32
+	fmt.Printf("%v, %T\n", r, r)
 
-	q := 3.14
-	q = 2.1e14 //even though this could be a float32, go always initializes floats as float64 unless specified
-	fmt.Printf("%v, %T\n", q, q)
-
-	var i complex64 = 1 + 2i
-	fmt.Printf("%v, %T\n", real(i), real(i))
-	fmt.Printf("%v, %T\n", imag(i), imag(i))
-
-	var t complex128 = complex(5, 12)
-	fmt.Printf("%v, %T\n", t, t)
 }
