cb3e97af4c1193dad4ae7ec055d24ad6a8432584
jestuber <jestuber@gmail.com>
2022-02-21 16:35:06 -0800

    trying out maps

--- src/github.com/jestuber/firstapp/main.go
+++ src/github.com/jestuber/firstapp/main.go
@@ -1,55 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	//grades := [3]int{97, 85, 93}
-	grades := [...]int{97, 85, 93} // ... gets length from data
-	fmt.Printf("Grades: %v\n", grades)
-
-	var students [3]string
-	fmt.Printf("Students: %v\n", students)
-	students[0] = "Lisa"
-	fmt.Printf("Students: %v\n", students)
-	fmt.Printf("Number of Students: %v\n", len(students))
-
-	a := [...]int{1, 2, 3}
-	b := &a
-	b[1] = 5
-	fmt.Println(a)
-	fmt.Println(b)
-
-	c := []int{1, 2, 3} // slice
-	d := c              // slices are reference types, this does not make a copy
-	d[1] = 5
-	fmt.Println(c)
-	fmt.Println(d)
-	fmt.Printf("Length: %v\n", len(c))
-	fmt.Printf("Capacity: %v\n", cap(d))
-
-	t := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	u := t[:]   // slice of all elements
-	v := t[3:]  // slice from 4th ele to end
-	x := t[:6]  // slice first 6 ele
-	y := t[3:6] // slice 4,5,and 6th ele
-	fmt.Println(t)
-	fmt.Println(u)
-	fmt.Println(v)
-	fmt.Println(x)
-	fmt.Println(y)
-
-	w := make([]int, 3, 100)
-	fmt.Println(w)
-	fmt.Printf("Length: %v\n", len(w))
-	fmt.Printf("Capacity: %v\n", cap(w))
-
-	z := []int{}
-	fmt.Println(z)
-	z = append(z, 1)
-	fmt.Println(z)
-	//z = append(z, 2, 3, 4, 5)
-	z = append(z, []int{2, 3, 4, 5}...)
-	fmt.Println(z)
+	statePopulations := make(map[string]int)
+	statePopulations = map[string]int{
+		"California": 39250017,
+		"Texas":      27862596,
+		"Ohio":       11614373,
+	}
+	pop, ok := statePopulations["typo"] // use comma ok to check if value is 0 or not in map
+	fmt.Println(pop, ok)
+	_, ok = statePopulations["Ohio"]
+	fmt.Println(ok)
 }
