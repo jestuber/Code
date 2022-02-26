17525fbc664a85d1cef930fd4e388943813aabd4
jestuber <jestuber@gmail.com>
2022-02-25 18:43:40 -0800

    simplest possible if statement

--- src/github.com/jestuber/firstapp/main.go
+++ src/github.com/jestuber/firstapp/main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
-type Animal struct {
-	Name   string `required max:"100"` // this is a tag
-	Origin string
-}
-
 func main() {
-	t := reflect.TypeOf(Animal{}) // need reflect library to get tag
-	field, _ := t.FieldByName("Name")
-	fmt.Println(field.Tag)
+	if true {
+		fmt.Println("The test is true")
+	}
+
 }
