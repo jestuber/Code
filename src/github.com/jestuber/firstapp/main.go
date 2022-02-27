2b8d487ce3625983b2d129d8bfe2d600abaeaf79
jestuber <jestuber@gmail.com>
2022-02-26 16:18:58 -0800

    if else

--- src/github.com/jestuber/firstapp/main.go
+++ src/github.com/jestuber/firstapp/main.go
@@ -5,14 +5,21 @@ import (
 )
 
 func main() {
-	statePopulations := map[string]int{
-		"California": 39250017,
-		"Texas":      27862596,
-	}
-	if pop, ok := statePopulations["Texas"]; ok {
-		fmt.Println(pop)
-	}
-	if pop, ok := statePopulations["Florida"]; ok {
-		fmt.Println(pop) // Initializer syntax
+	number := 50
+	guess := 30
+	if guess < 1 {
+		fmt.Println("The guess must be greater than 1!")
+	} else if guess > 100 {
+		fmt.Println("The guess must be less than 100!")
+	} else {
+		if guess < number {
+			fmt.Println("Too low")
+		}
+		if guess > number {
+			fmt.Println("Too high")
+		}
+		if guess == number {
+			fmt.Println("You got it!")
+		}
 	}
 }
